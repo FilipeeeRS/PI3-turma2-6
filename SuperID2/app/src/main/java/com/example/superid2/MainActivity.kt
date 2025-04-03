9e4fbaee2e2563a024da497c0b3d2b3bcf000927
Filipe RS <filipe.rs@puccampinas.edu.br>
2025-04-03 12:53:04 -0300

    tele inicial e tela de login
    tem q resolver a tela inicial e deixar mais bonita talvez

--- SuperID2/app/src/main/java/com/example/superid2/MainActivity.kt
+++ SuperID2/app/src/main/java/com/example/superid2/MainActivity.kt
@@ -9,17 +9,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.superid2.ui.theme.LoginScreen
+import com.example.superid2.ui.theme.TelaLogin
 import com.example.superid2.ui.theme.SuperID2Theme
+import com.example.superid2.ui.theme.TelaInicial
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen()
+            var mostrarTelaLogin by remember { mutableStateOf(false) }
+
+            if (mostrarTelaLogin) {
+                TelaLogin()
+            } else {
+                TelaInicial(onContinuarClick = { mostrarTelaLogin = true })
+            }
+            //pensar em outra forma
         }
     }
 }
